5307aecc8886787988211cd8e0ea0fe7441d469b
jsonwuyong <sundywuyong@outlook.com>
2018-03-22 23:19:26 +0800

    新增图片工具类

---
+++ src/main/java/com/ioto/util/PathUtil.java
@@ -0,0 +1,26 @@
+package com.ioto.util;
+
+/**
+ * Creater dengting on 2018/3/22
+ */
+public class PathUtil {
+
+    private static String seperator = System.getProperty("file.separator");
+
+    public static String getImgBasePath() {
+        String os = System.getProperty("os.name");
+        String basePath = "";
+        if (os.toLowerCase().startsWith("win")) {
+            basePath = "D:/projectdev/image";
+        } else {
+            basePath = "/Users/dengting/Documents/web-code/source/img";
+        }
+        basePath = basePath.replace("/", seperator);
+        return basePath;
+    }
+
+    public static String getShopImagePath(long shopId) {
+        String imagePath = "/upload/images/item/shop/" + shopId + "/";
+        return imagePath.replace("/", seperator);
+    }
+}
